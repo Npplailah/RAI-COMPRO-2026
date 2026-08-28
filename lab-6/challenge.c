136d834cac27dbe04a76fffdf53bbedb5da13663
Nopasin Plailaharn <69011934@kmitl.ac.th>
2026-08-28 15:32:48 +0700

    lab-6

--- lab-6/challenge.c
+++ lab-6/challenge.c
@@ -3,15 +3,14 @@
 struct Student {
     char name[21];
     int id;
-    float grades[4];
-    float average;
+    float gd[4];
+    float avg;
 };
 
 int main() {
     struct Student students[3];
-
     char subjects[4][10] = { "Math", "English", "Science", "History" };
-    float subjectAverage[4] = {0, 0, 0, 0};
+    float subjectAvg[4] = {0, 0, 0, 0};
 
     int topStudent = 0;
     int topSubject = 0;
@@ -20,42 +19,42 @@ int main() {
         printf("Enter name, ID, and 4 grades for student %d:\n", i + 1);
         scanf("%20s %d %f %f %f %f",
             students[i].name, &students[i].id,
-            &students[i].grades[0], &students[i].grades[1],
-            &students[i].grades[2], &students[i].grades[3]
+            &students[i].gd[0], &students[i].gd[1],
+            &students[i].gd[2], &students[i].gd[3]
         );
         
-        students[i].average = 0;
+        students[i].avg = 0;
         
         for (int j = 0; j < 4; j++) {
-            students[i].average += students[i].grades[j];
-            subjectAverage[j] += students[i].grades[j];
+            students[i].avg += students[i].gd[j];
+            subjectAvg[j] += students[i].gd[j];
         }
 
-        students[i].average /= 4;
+        students[i].avg /= 4;
         printf("\n");
     }
     
     for (int i = 1; i < 3; i++) {
-        if (students[i].average > students[topStudent].average) topStudent = i;
+        if (students[i].avg > students[topStudent].avg) topStudent = i;
     }
 
     printf("Student Averages:\n");
     for (int i = 0; i < 3; i++) {
-        printf("%s (ID: %d): %.2f\n", students[i].name, students[i].id, students[i].average);
+        printf("%s (ID: %d): %.2f\n", students[i].name, students[i].id, students[i].avg);
     }
 
-    printf("\nTop Student: %s with %.2f\n", students[topStudent].name, students[topStudent].average);
+    printf("\nTop Student: %s with %.2f\n", students[topStudent].name, students[topStudent].avg);
 
     printf("\nSubject Averages:\n");
     for (int i = 0; i < 4; i++) {
-        subjectAverage[i] /= 3;
-        printf("%s: %.2f\n", subjects[i], subjectAverage[i]);
+        subjectAvg[i] /= 3;
+        printf("%s: %.2f\n", subjects[i], subjectAvg[i]);
     }
 
     for (int j = 1; j < 4; j++) {
-        if (subjectAverage[j] > subjectAverage[topSubject]) topSubject = j;
+        if (subjectAvg[j] > subjectAvg[topSubject]) topSubject = j;
     }
 
-    printf("\nTop Subject: %s with average %.2f\n\n", subjects[topSubject], subjectAverage[topSubject]);
+    printf("\nTop Subject: %s with average %.2f\n\n", subjects[topSubject], subjectAvg[topSubject]);
     return 0;
 }
